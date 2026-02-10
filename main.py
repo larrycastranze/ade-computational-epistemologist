fb81a8b8ce10a86ab169c4fb54505141540d27af
larrycastranze <larrycastranze@gmail.com>
2026-02-10 16:14:52 -0500

    Create main.py with simulation runner code

---
+++ main.py
@@ -0,0 +1,7 @@
+def simulation_runner():
+    """Function to run the simulation."""
+    # Simulate some process here
+    print("Simulation is running...")
+
+if __name__ == "__main__":
+    simulation_runner()
